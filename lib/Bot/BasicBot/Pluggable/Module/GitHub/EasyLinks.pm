c1190d40f474f3d5c27cae481faadc35348b7c2d
Ailin Nemui <ailin@devio.us>
2017-03-08 15:15:04 +0000

    [local] hardcode an ignore rule for other github bots, whose nicks star in this channels with "Not-"

--- lib/Bot/BasicBot/Pluggable/Module/GitHub/EasyLinks.pm
+++ lib/Bot/BasicBot/Pluggable/Module/GitHub/EasyLinks.pm
@@ -70,6 +70,8 @@ sub said {
     
     return unless $pri == 2;
 
+#    return if $mess->{body} =~ m{://git};
+    return if $mess->{who} =~ /^Not-/;
 
     # Loop through matching things in the message body, assembling quick links
     # ready to return.
